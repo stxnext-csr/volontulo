1c467838fc3f97dd61ef0c265f0a64d7436ce80a
Tomasz Magulski <tomasz.magulski@stxnext.pl>
2015-09-14 18:31:07 +0200

    #25 - offer's activation made by admin (+plus mostly mockuped login/logout)

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+
+from django.contrib import auth
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
@@ -11,11 +13,36 @@ def index(request):
     return HttpResponse(u"Welcome in volontulo app.")
 
 
+def login(request):
+    if request.method == 'GET':
+        return render(request, "volontulo/login.html")
+    username = request.POST['login']
+    password = request.POST['password']
+    user = auth.authenticate(username=username, password=password)
+    auth.login(request, user)
+    return HttpResponse(u"it's login.")
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponse(u"it's logout.")
+
+
 def list_offers(request):
-    offers = models.Offer.objects.all()
+    if (
+        request.user.is_authenticated() and
+        models.UserProfile.objects.get(user=request.user).is_admin
+    ):
+        offers = models.Offer.objects.all()
+    else:
+        offers = models.Offer.objects.filter(status='ACTIVE')
     return render(request, "volontulo/list_offers.html", context={
         'offers': offers,
     })
+
+
+def activate_offer(request, offer_id):
+    return HttpResponse('dadasdas')
 
 
 def show_offer(request, offer_id):
