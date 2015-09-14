7649d47b4bf458ec71eaa1a76a994759e029b387
Tomasz Magulski <tomasz.magulski@stxnext.pl>
2015-09-14 14:33:16 +0200

    #39 - show details of offer

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    pass
+
+
+class Offer(models.Model):
+    organization = models.ForeignKey(Organization)
+    description = models.TextField()
+    requirements = models.TextField()
+    time_commitment = models.TextField()
+    benefits = models.TextField()
--- views.py
+++ views.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+
+def index(request):
+    return HttpResponse(u"Welcome in volontulo app.")
+
+
+def show_offer(request, id_):
+    offer = models.Offer.objects.get(id=id_)
+    return render(request, "volontulo/show_offer.html", context={
+        'offer': offer,
+    })
