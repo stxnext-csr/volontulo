54c697f809b3df6da4ce20b8e5025be536b61b96
Tomasz Magulski <tomasz.magulski@stxnext.pl>
2015-09-14 15:31:13 +0200

    #38 - list of offers visible for volonteers

--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ from . import views
 
 urlpatterns = [
     url('^$', views.index, name='index'),
+    url('offers/list', views.list_offers, name='list_offers'),
     url('offers/show/(?P<offer_id>[0-9]+)$', views.show_offer, name='show_offer'),
 ]
--- views.py
+++ views.py
@@ -11,6 +11,13 @@ def index(request):
     return HttpResponse(u"Welcome in volontulo app.")
 
 
+def list_offers(request):
+    offers = models.Offer.objects.all()
+    return render(request, "volontulo/list_offers.html", context={
+        'offers': offers,
+    })
+
+
 def show_offer(request, offer_id):
     offer = get_object_or_404(models.Offer, id=offer_id)
     return render(request, "volontulo/show_offer.html", context={
