124778fc29991f10f70dc0fe11bd8b8c46c9a487
Filip Górczyński <filip.gorczynski@gmail.com>
2015-09-22 12:33:40 +0200

    #7 Organization activity - list of active and not active offers for currently viewing ~organization

--- volontulo/views.py
+++ volontulo/views.py
@@ -283,6 +283,7 @@ def organization_form(request):
 def organization_view(request, slug, organization_id):
     u"""View responsible for viewing organization."""
     org = get_object_or_404(Organization, id=organization_id)
+    offers = Offer.objects.filter(organization_id=organization_id)
     if request.method == 'POST':
         form = VolounteerToOrganizationContactForm(request.POST)
         if form.is_valid():
@@ -318,6 +319,7 @@ def organization_view(request, slug, organization_id):
                 {
                     'organization': org,
                     'contact_form': form,
+                    'offers': offers,
                 },
             )
     form = VolounteerToOrganizationContactForm()
@@ -327,6 +329,7 @@ def organization_view(request, slug, organization_id):
         {
             'organization': org,
             'contact_form': form,
+            'offers': offers,
         },
     )
 
