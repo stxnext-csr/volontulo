4df68255d660da46252cc60d1ab653e3908b3efa
Tomasz Magulski <tomasz.magulski@gmail.com>
2015-09-26 21:07:20 +0200

    #178 - gulp command watch for live reloading + package.json for simple npm install command

--- volontulo/gulpfile.js
+++ volontulo/gulpfile.js
@@ -1,8 +1,15 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var watchify = require('watchify');
 
 gulp.task('build', function() {
-    gulp.src('frontend/*')
-        .pipe(gulp.dest('static/volontulo/'));
+    gulp.src('./frontend/**/*')
+        .pipe(gulp.dest('./static/volontulo/'));
+});
+
+gulp.task('watch', ['build'], function() {
+    gulp.watch('./frontend/**/*', ['build']);
+    gutil.log(gutil.colors.bgGreen('Watching for changes...'));
 });
