08f806ce055195f50a32c6cb12a9ec7ffbf1f2a0
Filip Górczyński <filip.gorczynski@gmail.com>
2015-11-16 11:51:11 +0100

    #320 fixes from review - redundant database test settings

--- volontulo_org/settings/test_settings.py
+++ volontulo_org/settings/test_settings.py
@@ -7,14 +7,4 @@ u"""
 
 from .base import *
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
