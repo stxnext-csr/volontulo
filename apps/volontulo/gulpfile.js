14c67ebf39c5274b6bd796302a432890a7021958
Piotr Rorot <piotr.rorot@gmail.com>
2016-01-18 16:48:08 +0100

    #402 added new task iconify to gulp and integrated its output into scss,

--- apps/volontulo/gulpfile.js
+++ apps/volontulo/gulpfile.js
@@ -3,7 +3,29 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var sass = require('gulp-sass');
+var iconify = require('gulp-iconify');
 
+gulp.task('build_icons', function() {
+    iconify({
+        src: './frontend/icons/*.svg',
+        pngOutput: './static/volontulo/img/icons/png',
+        cssDisabled: true,
+        cssOutput: 'false',
+        scssOutput: './frontend/scss/iconify',
+        styleTemplate: './frontend/icons/template/icon_gen.scss.mustache',
+        defaultWidth: '32px',
+        defaultHeight: '32px',
+        svgoOptions: {
+            enabled: true,
+            options: {
+                plugins: [
+                    { removeUnknownsAndDefaults: true },
+                    { mergePaths: false }
+                ]
+            }
+        }
+    });
+});
 
 gulp.task('build_fonts', function() {
     gulp.src('./frontend/fonts/**/*')
@@ -24,6 +46,9 @@ gulp.task('build_scss', function() {
     gulp.src('./frontend/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./static/volontulo/css/'));
+});
+
+gulp.task('build:svg', ['build_icons'], function() {
 });
 
 gulp.task('build', ['build_fonts', 'build_img', 'build_javascripts', 'build_scss'], function() {
