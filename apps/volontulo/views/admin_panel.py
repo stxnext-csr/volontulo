2af7eb7d1a040fb842696b778c11fd386a59a074
Michal Ziach <ziachmichal@gmail.com>
2016-02-26 22:21:04 +0100

    #464 pep8

--- apps/volontulo/views/admin_panel.py
+++ apps/volontulo/views/admin_panel.py
@@ -3,9 +3,10 @@
 """
 from django.shortcuts import render
 
+
 def main_panel(request):
 
-	return render(
-		request,
-		'admin/list_offers.html'
-	)
+    return render(
+        request,
+        'admin/list_offers.html'
+    )
