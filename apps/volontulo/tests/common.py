9dcf2680293487c3caf3e9c401e7e4750ad954fb
Tomasz Magulski <tomasz.magulski@stxnext.pl>
2016-02-26 14:23:53 +0100

    fixing travis (unittest) issue

--- apps/volontulo/tests/common.py
+++ apps/volontulo/tests/common.py
@@ -71,6 +71,7 @@ def initialize_filled_volunteer_and_organization():
         'volunteer2'
     )
     volunteer_user2.save()
+    UserProfile.objects.create(user=volunteer_user2)
 
     # create organization user to create offers
     organization2 = Organization.objects.create(name=u'Organization 2')
