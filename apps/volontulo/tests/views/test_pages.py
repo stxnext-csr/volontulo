ae57548ef21ba3a87ca3492735b0478197725a96
krzysztofmadejski <krzysztof@madejscy.pl>
2016-02-26 19:44:44 +0100

    closes #450, waiting for #451 for tests to pass

--- apps/volontulo/tests/views/test_pages.py
+++ apps/volontulo/tests/views/test_pages.py
@@ -100,3 +100,8 @@ class TestPages(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'pages/regulations.html')
         self.assertContains(response, u'Regulamin')
+
+    def test_office_subpage(self):
+        u"""Test office subpage."""
+        response = self.client.get('/office')
+        self.assertEqual(response.status_code, 200)
