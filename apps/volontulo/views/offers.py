6986a71cd418345f7063743e78b779b3e41e05f1
Patryk Gruszka <patryk.gruszka@stxnext.pl>
2016-02-26 22:08:47 +0100

    Redirect non existing users to registration page.

--- apps/volontulo/views/offers.py
+++ apps/volontulo/views/offers.py
@@ -393,22 +393,18 @@ class OffersJoin(View):
     @staticmethod
     @correct_slug(Offer, 'offers_join', 'title')
     def post(request, slug, id_):  # pylint: disable=unused-argument
-        u"""View responsible for saving join for particular offer."""
+        """View responsible for saving join for particular offer."""
         form = OfferApplyForm(request.POST)
         offer = Offer.objects.get(id=id_)
         if form.is_valid():
             if request.user.is_authenticated():
                 user = request.user
             else:
-                try:
-                    user = User.objects.create_user(
-                        username=request.POST.get('email'),
-                        email=request.POST.get('email'),
-                        password=User.objects.make_random_password(),
-                    )
-                    profile = UserProfile(user=user)
-                    profile.save()
-                except IntegrityError:
+                user = User.objects.filter(
+                    email=request.POST.get('email')
+                ).exists()
+
+                if user:
                     messages.info(
                         request,
                         'Zaloguj się, aby zapisać się do oferty.'
@@ -416,6 +412,12 @@ class OffersJoin(View):
                     return redirect(
                         reverse('login') + '?next={}'.format(request.path)
                     )
+                else:
+                    messages.info(
+                        request,
+                        'Zarejestruj się, aby zapisać się do oferty.'
+                    )
+                    return redirect('register')
 
             has_applied = Offer.objects.filter(
                 volunteers=user,
