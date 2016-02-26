05e3867ba2060a64f2a5b278bdd7713a20ec6c53
Radosław Roszkowiak <radoslaw@roszkowiak.pl>
2016-02-26 19:52:17 +0100

    [474] is_avatar removed from "/me" edit profile page

--- apps/volontulo/tests/forms/test_edit_profile.py
+++ apps/volontulo/tests/forms/test_edit_profile.py
@@ -83,3 +83,14 @@ class TestEditProfileForm(TransactionTestCase):
         )
         form = EditProfileForm(data=form_data)
         self.assertRaises(ValidationError, form.is_valid)
+
+    def test__no_email_field_on_edit_profile_form(self):
+        u"""Test if Is Avatar field is not visible."""
+
+        self.client.post('/login', {
+            'email': u'volunteer1@example.com',
+            'password': 'volunteer1',
+        })
+        response = self.client.get('/me')
+        # pylint: disable=no-member
+        self.assertNotContains(response, u'is_avatar')
