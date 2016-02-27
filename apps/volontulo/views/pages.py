e1fac8b25c9d555674a5b36baeac6fa2bf3a0df2
mpuhacz <marcinpuhacz@gmail.com>
2016-02-27 01:33:55 +0100

    pylint

--- apps/volontulo/views/pages.py
+++ apps/volontulo/views/pages.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
 from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import redirect
 from django.views.generic.detail import DetailView
@@ -10,16 +12,19 @@ from django.views.generic.list import ListView
 from apps.volontulo.models import Page
 
 
+# pylint: disable=too-many-ancestors;missing-docstring
 class PageList(ListView):
     model = Page
     template_name = 'pages/page_list.html'
 
 
+# pylint: disable=too-many-ancestors;missing-docstring
 class PageDetails(DetailView):
     model = Page
     template_name = 'pages/page_detail.html'
 
 
+# pylint: disable=too-many-ancestors;missing-docstring
 class PageCreate(CreateView):
     model = Page
     fields = (
@@ -31,12 +36,14 @@ class PageCreate(CreateView):
     success_url = reverse_lazy('pages_list')
 
     def form_valid(self, form):
+        # pylint: disable=attribute-defined-outside-init
         self.object = form.save(commit=False)
         self.object.author = self.request.user.userprofile
         self.object.save()
         return redirect(self.get_success_url())
 
 
+# pylint: disable=too-many-ancestors;missing-docstring
 class PageEdit(UpdateView):
     model = Page
     fields = (
@@ -48,6 +55,7 @@ class PageEdit(UpdateView):
     success_url = reverse_lazy('pages_list')
 
 
+# pylint: disable=too-many-ancestors;missing-docstring
 class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy('pages_list')
